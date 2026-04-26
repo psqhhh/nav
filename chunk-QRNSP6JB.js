bb98d30b993342c0da32d0279e74b4d7d6893fae
psqhhh <psqhhh@163.com>
2026-04-26 23:11:25 +0000

    Deploying to gh-pages from @ psqhhh/nav@61d0fbef61f4ba70f55aae97a6f5cd739ccc7c55 🚀

--- chunk-QRNSP6JB.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./chunk-N7TR4K5N.js";import{Ab as e,sb as r}from"./chunk-QITYORFX.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
